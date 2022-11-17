6019ee6e54ac1c25a20d7b64797c1bfeb8ba2b76
Rustam Kadimov <kadim_r@mail.ru>
2022-11-17 12:53:59 +0500

    Initial commit

---
+++ src/main/java/com/example/tacocloud/TacoCloudCh05SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudCh05SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudCh05SecurityApplication.class, args);
+    }
+
+}
